27068c4b4ef061180abb7676d0976ba8ed32c9a6
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-05-18 17:49:29 +0200

    second style

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
-// Array of Deck of Card Images
-const cardPlate = ["box.png", "box.png", "clock.png", "clock.png", "instagram.png", "instagram.png", "linkedin.png", "linkedin.png", "musik.png", "musik.png", "new.png", "new.png", "sprechblase.png", "sprechblase.png", "suche.png", "suche.png"];
+
+const cardPlate = ["box.png", "box.png", "clock.png", "clock.png", "instagram.png", "instagram.png", "linkedin.png", "linkedin.png", "tinder.png", "tinder.png", "new.png", "new.png", "sprechblase.png", "sprechblase.png", "twitter.png","twitter.png"];
 const slot = document.querySelector(".slot");
+
 
 let opened = [];
 
 let matched = [];
 
 
+
+
+
 const modal = document.getElementById("modal");
 const reset = document.querySelector(".reset-btn");
-const playAgain = document.querySelector(".play-again-btn");
+const playAgain = document.querySelector(".try-again");
 
  
 const movesCount = document.querySelector(".moves-counter");
 let moves = 0;
 
-const remainingMove = document.querySelector(".remaining-moves");
-let totalMoves = 24
-
-const star = document.getElementById("star-rating").querySelectorAll(".star");
-let starCount = 3;
-
+// const star = document.getElementById("star-rating").querySelectorAll(".star");
+// let starCount = 3;
 
 const timeCounter = document.querySelector(".timer");
 
@@ -30,9 +30,36 @@ let minutes = 0;
 let seconds = 0;
 let timeStart = false;
 
+//////////////////////////////////// audio Section////////////////////////////////////////////////////////
+function playSound(url){
+	new Audio(url).play();
+	}
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+function matchSound (){
+let myAudio = document.createElement("audio");
+	myAudio.src = "/audio/achievement.wav";
+	myAudio.play();
+}
+;
+function winSound(){
+	let winAudio = document.createElement("audio");
+	winAudio.src = "/audio/winning-swoosh.wav";
+	winAudio.play();
+}
 
+function nomatchSound(){
+	let wrongMatchAudio = document.createElement("audio");
+	wrongMatchAudio.src = "/audio/wrong.wav";
+	wrongMatchAudio.play();
+}
+function backgroundAudio(){
+	let backgroundAudio = document.createElement("audio");
+	backgroundAudio.src = "/audio/backgroundmusic.mp3";
+	backgroundAudio.play();
+	backgroundAudio.volume = 0.7;
+	backgroundAudio.loop=true
+}
+////////////////////////////////////////END OF AUDIO/////////////////////////////////
 function shuffle(array) {
   let currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -46,16 +73,10 @@ function shuffle(array) {
   return array;
 }
 
-/*
-Start Game: Shuffle the deck, create <li> tags and <img> 
-tags and append to the deck <ul> with the new shuffled content
-*/
 function startGame() {
-	
 	const shuffledDeck = shuffle(cardPlate); 
-	
 	for (let i = 0; i < shuffledDeck.length; i++) {
-		
+
 		const liTag = document.createElement('li');
 		liTag.classList.add('card');
 		
@@ -63,39 +84,33 @@ function startGame() {
 		
 		liTag.appendChild(addImage);
 		
-		addImage.setAttribute("src", "https://github.com/benifbond/memory-game-personal-project/tree/master/images" + shuffledDeck[i] + "?raw=true");
+		addImage.setAttribute("src", "../images/" + shuffledDeck[i]);
 	
-		addImage.setAttribute("alt", "favicon");
+		addImage.setAttribute("alt", "images");
 	
 		slot.appendChild(liTag);
+
 	}
+	
 }
 
 startGame();
 
-/*
-Remove all child nodes from the deck <li> tags and
-<img> tags.  To be called in set everything function only
-*/
+
+
 function removeCard() {
-	// As long as <ul> deck has a child node, remove it
 	while (slot.hasChildNodes()) {
 		slot.removeChild(slot.firstChild);
 	}
 }
 
-/*
-set Timer
-*/
 function timer() {
-
 	time = setInterval(function() {
 		seconds++;
 			if (seconds === 60) {
 				minutes++;
 				seconds = 0;
 			}
-		// Update the timer in HTML with the time it takes the user to play the game
 		timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: " + minutes + " Mins " + seconds + " Secs" ;
 	}, 1000);
 }
@@ -107,165 +122,98 @@ function stopTime() {
 
 
 function resetEverything() {
-	// Stop time, reset the minutes and seconds update the time inner HTML
 	stopTime();
 	timeStart = false;
 	seconds = 0;
 	minutes = 0;
 	timeCounter.innerHTML = "<i class='fa fa-hourglass-start'></i>" + " Timer: 00:00";
-	// Reset star count and the add the class back to show stars again
-	
-	// Reset moves count and reset its inner HTML
+
 	moves = 0;
 	movesCount.innerHTML = 0;
-	// Clear both arrays that hold the opened and matched cards
 	matched = [];
 	opened = [];
-	// Clear the deck
 	removeCard();
-	// Create a new deck
 	startGame();
 }
 
 
 function movesCounter() {
-	
 	movesCount.innerHTML ++;
-	
 	moves ++;
 }
 
 
-function remainingMoves(){
-	if (totalMoves-movesCounter > 0){
+// function starRating() {
+// 	if (moves === 14) {
+// 		star[2].firstElementChild.classList.remove("fa-star");
+// 		starCount--;
+// 	}
+// 	if (moves === 18) {
+// 		star[1].firstElementChild.classList.remove("fa-star");
+// 		starCount--;
+// 	}
+//}
 
-	}
-}
-/*
-Update the star rating.  Depending on the number of
-moves the user completes the game, the stars will decrease
-with the more moves the user takes.
-*/
-function starRating() {
-	if (moves === 14) {
-		// First element child is the <i> within the <li>
-		star[2].firstElementChild.classList.remove("fa-star");
-		starCount--;
-	}
-	if (moves === 18) {
-		star[1].firstElementChild.classList.remove("fa-star");
-		starCount--;
-	}
-}
-
-/*
-Compare two cards to see if they match or not
-*/
 function compareTwo() {
-	// When there are 2 cards in the opened array
 	if (opened.length === 2) {
-  		// Disable any further mouse clicks on other cards
   		document.body.style.pointerEvents = "none";
   }
-	// Compare the two images src
 	if (opened.length === 2 && opened[0].src === opened[1].src) {
-		// If matched call match()
 		match();
-		// console.log("It's a Match!");
 	} else if (opened.length === 2 && opened[0].src != opened[1].src) {
-		// If No match call noMatch()
 		noMatch();
-		// console.log("NO Match!");
+		
 	}
 }
 
-/*
-If the two cards match, keep the cards open and
-apply class of match
-*/ 
+
 function match() {
-	/* Access the two cards in opened array and add
-	the class of match to the images parent: the <li> tag
-	*/
 	setTimeout(function() {
 		opened[0].parentElement.classList.add("match");
 		opened[1].parentElement.classList.add("match");
-		// Push the matched cards to the matched array
 		matched.push(...opened);
-		// Allow for further mouse clicks on cards
 		document.body.style.pointerEvents = "auto";
-		// Check to see if the game has been won with all 8 pairs
 		winGame();
-		// Clear the opened array
+		matchSound ();
 		opened = [];
 	}, 600);
-	// Call movesCounter to increment by one
 	movesCounter();
-	starRating();
+	//starRating();
 }
 
-/*
-If the two cards do not match, remove the cards
-from the opened array and flip the cards back over by
-removing the flip class.
-*/
+
 function noMatch() {
-	/* After 700 miliseconds the two cards open will have
-	the class of flip removed from the images parent element <li>*/
 	setTimeout(function() {
-		// Remove class flip on images parent element
 		opened[0].parentElement.classList.remove("flip");
 		opened[1].parentElement.classList.remove("flip");
-		// Allow further mouse clicks on cards
 		document.body.style.pointerEvents = "auto";
-		// Remove the cards from opened array
 		opened = [];
 	}, 700);
-	// Call movesCounter to increment by one
+	nomatchSound();
 	movesCounter();
-	starRating();
+	// starRating();
 }
 
-/*
-Get stats on the time, how many moves, and star rating
-for the end game and update the modal with these stats
-*/
-function AddStats() {
-	// Access the modal content div
-	const stats = document.querySelector(".modal-content");
-	// Create three different paragraphs
-	for (let i = 1; i <= 3; i++) {
-		// Create a new Paragraph
-		const statsElement = document.createElement("p");
-		// Add a class to the new Paragraph
-		statsElement.classList.add("stats");
-		// Add the new created <p> tag to the modal content
-		stats.appendChild(statsElement);
-	}
-	// Select all p tags with the class of stats and update the content
-	let p = stats.querySelectorAll("p.stats");
-			// Set the new <p> to have the content of stats (time, moves and star rating)
-		p[0].innerHTML = "Time to complete: " + minutes + " Minutes and " + seconds + " Seconds";
-		p[1].innerHTML = "Moves Taken: " + moves;
-		p[2].innerHTML = "Your Star Rating is: " + starCount + " out of 3";
-}
+// function AddStats() {
+// 	const stats = document.querySelector(".modal-content");
+// 	for (let i = 1; i <= 3; i++) {
+// 		const statsElement = document.createElement("p");
+// 		statsElement.classList.add("stats");
+// 		stats.appendChild(statsElement);
+// 	}
+// 	let p = stats.querySelectorAll("p.stats");
+// 		p[0].innerHTML = "Time to complete: " + minutes + " Minutes and " + seconds + " Seconds";
+// 		p[1].innerHTML = "Moves Taken: " + moves;
+// 		p[2].innerHTML = "Your Star Rating is: " + starCount + " out of 3";
+//}
 
-/*
-Display the modal on winning the game
-Help with the modal from:
-https://www.w3schools.com/howto/howto_css_modals.asp
-*/
+
 function displayModal() {
-// Access the modal <span> element (x) that closes the modal
 const modalClose = document.getElementsByClassName("close")[0];
-	// When the game is won set modal to display block to show it
 	modal.style.display= "block";
-
-	// When the user clicks on <span> (x), close the modal
 	modalClose.onclick = function() {
 		modal.style.display = "none";
 	};
-// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
 		if (event.target == modal) {
 			modal.style.display = "none";
@@ -273,76 +221,68 @@ const modalClose = document.getElementsByClassName("close")[0];
 	};
 }
 
-/*
-Check the length of the matched array and if there
-are 8 pairs 16 cards all together then the game is won.
-Stop the timer update the modal with stats and show the modal
-*/
+
 function winGame() {
 	if (matched.length === 16) {
 		stopTime();
-		AddStats();
-		displayModal();
+		//AddStats();
+		//displayModal();
+		winSound();
 	}
 }
 
-/*----------------------------------  
-Main Event Listener
-------------------------------------*/
-/*
-Event Listener if a card <li> is clicked
-call flipCard()
-*/
+
 slot.addEventListener("click", function(evt) {
 	if (evt.target.nodeName === "LI") {
-		// To console if I was clicking the correct element 
-		console.log(evt.target.nodeName + " Was clicked");
-		// Start the timer after the first click of one card
-	// Executes the timer() function
+		
 		if (timeStart === false) {
 			timeStart = true; 
 			timer();
 		}
-		// Call flipCard() function
 		flipCard();
 	}
-
-	//Flip the card and display cards img
 	function flipCard() {
-		// When <li> is clicked add the class .flip to show img
 		evt.target.classList.add("flip");
-		// Call addToOpened() function
 		addToOpened();
 	}
-	 
-	//Add the fliped cards to the empty array of opened
+	
 	function addToOpened() {
-		/* If the opened array has zero or one other img push another 
-		img into the array so we can compare these two to be matched
-		*/
-		if (opened.length === 0 || opened.length === 1) {
-			// Push that img to opened array
+		if (opened.length === 0 || opened.length === 1) {	
 			opened.push(evt.target.firstElementChild);
 		}
-		// Call compareTwo() function
 		compareTwo();
 	}
-}); //Event Listener
+	
+}); 
 
-/*----------------------------------  
-Restart Buttons
-------------------------------------*/
-/*
-Event Listener to listen for a click on the reset
-button, once clicked call resetEverything()
-*/
 reset.addEventListener('click', resetEverything);
 
-/*
-Event Listener to listen for a click on the play
-again button, once clicked call resetEverything()
-*/
+
+
 playAgain.addEventListener('click',function() {
-	modal.style.display = "none";
 	resetEverything();
-});
+	playSound()
+}) 
+
+/////////////////////////////////// Greeting ///////////////////////////
+
+let greetinFunc =    function greetingMessages() {
+	const date = new Date();
+	const currentHour = date.getHours();
+	
+	let greeting = "";
+	if (currentHour > 0 && currentHour < 12) {
+	  greeting = "Good morning";
+	 
+	} else if (currentHour > 12 && currentHour < 18) {
+	  greeting = "Good Afternoon";
+	  
+	} else {
+	  greeting = "Good Evening";
+	  
+	}return greeting;
+	
+}
+let greetingMessge = document.getElementById('greeting').innerHTML = greetinFunc()
+ 
+let trySomethingOut = document.getElementById('trySomethingOut').innerText= "woo! ready for the challenge? I still don't wanna tell you that........ sorry! I wouldn't tell the public your memory capacity! mine aint that good either. "
